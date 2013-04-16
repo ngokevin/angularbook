77b4637fa99e751be97cd99dd1e1abb1ae5df592
Kevin Ngo <ngoke@mozilla.com>
2013-04-16 02:38:11 -0700

    dependency injection

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('helloWorldApp', []);
 
-app.controller('HelloWorldCtrl', function($scope) {
+app.controller('HelloWorldCtrl', ['$scope', function($scope) {
     $scope.targetOfSalutation = 'World';
-});
+}]);
